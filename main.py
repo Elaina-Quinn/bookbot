eb7a06bf689fe5ad9706b6cf74abf819532b3bf4
Elaina-Quinn <elaina.riggert@gmail.com>
2024-12-09 17:50:46 -0800

    added character counter

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+# new function to count number of each character in book
+# go through each word and add character to running count
+    # make a dictionary
+
+# counts words in book
 def word_count(book):
     words = book.split()
 
     return len(words)
+
+# counts how many of each character there is
+def character_counter(lowercase_book):
+    character_count = {}
+
+    for i in lowercase_book:
+        if i in character_count.keys():
+            character_count[i] = character_count[i] + 1
+        elif i.isalpha():
+            character_count[i] = 1
+
+    return character_count
+
 
 def main():
     file_contents = ""
@@ -9,6 +27,8 @@ def main():
         file_contents = book.read()
 
     print (word_count(file_contents))
+
+    print (character_counter(file_contents.lower()))
 
 
 main()
